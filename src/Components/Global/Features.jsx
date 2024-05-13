26b50bb7ac93f56e660724177b52666cdb282d12
henrytrust1111 <henrytrust1111@gmail.com>
2024-05-13 19:05:55 +0100

    first commit4

--- src/Components/Global/Features.jsx
+++ src/Components/Global/Features.jsx
@@ -1,16 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState={
-   
+    data: [],
 };
 
 const ProductState = createSlice({
     name:"product",
     initialState,
     reducers:{
-
+        DB:(state, {payload})=>{
+            state.data = payload
+        }
     }
 })
 
-export const {} = ProductState.actions;
+export const {DB} = ProductState.actions;
 export default ProductState.reducer;
