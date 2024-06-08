d04a04ecae8204891961b7070f4dc86490555252
henrytrust1111 <henrytrust1111@gmail.com>
2024-06-08 20:01:47 +0100

    feedback finished3

--- src/Components/Auth/Login/DesktopView/LoginHeader.jsx
+++ src/Components/Auth/Login/DesktopView/LoginHeader.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
-import logo from "/images/5square_Logo.jpg";
-import { Wrapper } from "../../../Container/Container";
+import logo from "/images/5-Square Logo.png";
 
 const LoginHeader = ({ identityVerification, links, style }) => {
   return (
     <header className="w-full h-20 flex justify-between items-center px-4" style={style}>
      <div className="w-11/12 h-full flex justify-between items-center px-4   text-3xl font-bold ">
      <div className="h-20 flex items-center w-[60%]">
-        <div className="h-[70%] w-[40%]">
-          <img src={logo} alt="Logo" className="w-full h-full object-cover" />
+        <div className="h-[70%] w-[30%]">
+          <img src={logo} alt="Logo" className="w-full h-full object-contain" />
         </div>
         {identityVerification && (
-          <div className="text-3xl font-normal w-[60%] text-[#333]">
+          <div className="text-3xl font-normal w-[70%] text-[#333]">
             {identityVerification}
           </div>
         )}
@@ -41,21 +40,3 @@ LoginHeader.propTypes = {
 
 export default LoginHeader;
 
-
-
-// import React from "react";
-// import logo from "/images/5square_Logo.jpg"
-
-// const LoginHeader = () => {
-//   return (
-//     <header className="w-full h-20 flex-center ">
-//       <div className="w-11/12 h-full text-3xl font-bold flex items-center ">
-//       <div className="h-[70%] w-[30%] ">
-//        <img src={logo} alt="Logo" className="w-full h-full object-cover" />
-//        </div>
-//       </div>
-//     </header>
-//   );
-// };
-
-// export default LoginHeader;
